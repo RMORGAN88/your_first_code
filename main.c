ccb84e0028b67f4276d96393ca243927dee4d1a7
RMORGAN88 <49418410+RMORGAN88@users.noreply.github.com>
2019-04-08 22:20:14 -0400

    Morgan Rob
    Updated by Morgan88

--- main.c
+++ main.c
@@ -1,9 +1,23 @@
-int main()
-{
-  int i;
-  for (i = 0; i < 10; i++)
-  {
-    printf("Holberton\n");
-  }
-  return 0;
-}
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton" 
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton" 
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton" 
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton" 
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton" 
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton"
+"Hello Holberton" 
