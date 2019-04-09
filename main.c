b57519d4cdf89e9c45945f7be5133eb4846b60c1
RMORGAN88 <49418410+RMORGAN88@users.noreply.github.com>
2019-04-08 20:23:00 -0400

    Update main.c
    Update by AirMorgan

--- main.c
+++ main.c
@@ -3,7 +3,30 @@ int main()
   int i;
   for (i = 0; i < 10; i++)
   {
-    printf("Holberton\n");
+    printf("HELLO HOLBERTON;
   }
   return 0;
-}
+
+           int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf;"HELLO HOLBERTON;
+  }
+  return 0;
+}int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("HELLO HOLBERTON;
+  return 0;
+          int main()
+{
+  int i;
+  for (i = 0; i < 10; i++)
+  {
+    printf("HELLO HOLBERTON;
+  }
+  return 0;
